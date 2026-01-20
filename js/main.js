138c371d05fbee24659ffaf4837ae4ccc961c1ae
kieeran <giakietgx@gmail.com>
2026-01-20 13:07:51 +0700

    add partner logos

---
+++ js/main.js
@@ -0,0 +1,18 @@
+import { partnerLogoBasePath, productList, partnerLogos } from "./data.js";
+
+/**
+ * Partner logos
+ *
+ */
+$(function () {
+  const container = document.getElementById("partner-logo-list");
+
+  partnerLogos.forEach((logo) => {
+    const img = document.createElement("img");
+
+    img.src = partnerLogoBasePath + logo.fileName;
+    img.alt = logo.alt;
+    img.classList.add("logo-ticker-image");
+    container.appendChild(img);
+  });
+});
